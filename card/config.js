05c1cb4d0751be3b144446a2bb9b033ce90e231d
NguyenThuy <tn262995@gmail.com>
2022-02-28 23:25:14 +0700

    add index.html

--- card/config.js
+++ card/config.js
@@ -4,7 +4,7 @@ const eventName = 'Happy Birthday';
 const titleCard = 'Celebrating the 65th anniversary of the establishment of HUST.';
 const contentCard = '</br>Happy 65th birthday. Wish HUST will always be the first prestigious and influential science and technology university in Vietnam!</br></br> ONE LOVE ONE FUTURE💞💞';
 
-const giftImage = 'BK.png';
+const giftImage = './img/BK.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
 
