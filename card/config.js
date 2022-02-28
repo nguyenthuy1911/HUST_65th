952f7e064e94b559f1f9f4b9f35c81e71f897096
NguyenThuy <tn262995@gmail.com>
2022-02-28 23:06:27 +0700

    first commit

---
+++ card/config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'HUST';
+const giftUrl = 'https://www.facebook.com/Aippi1021611040206';
+const eventName = 'Happy Birthday';
+const titleCard = 'Celebrating the 65th anniversary of the establishment of HUST.';
+const contentCard = '</br>Happy 65th birthday. Wish HUST will always be the first prestigious and influential science and technology university in Vietnam!</br></br> ONE LOVE ONE FUTURE💞💞';
+
+const giftImage = 'BK.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
+
---
+++ card/present.js
@@ -0,0 +1,53 @@
+
+var to = nameGirl;
+var gift_url = giftUrl;
+var gift_image_url = giftImage || giftImageBase64;
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+document.addEventListener("click", function(e){
+  document.getElementById('Welcome').classList.add('welcome-hide');
+  document.getElementById('parent').classList.add('p-height');
+  document.getElementById('above-fold').classList.add('above-fold-show');
+
+}, false);
+
+function init() {
+  var graphElem = document.querySelector('.present-box > .side.top .to');
+  graphElem.setAttribute('data-before', eventName);
+  document.querySelector('#card .title-card').innerHTML = `💘${titleCard}💘`;
+  document.querySelector('#card .content-card').innerHTML = `${contentCard}`;
+  document.querySelector('#card .honey').setAttribute('src', `${giftImage}`);
+
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show');
+    document.getElementById('right-lt').classList.add('right-lt-show');
+  }, false);  
+  nametag.innerText = to;
+}
+
+init();
+
